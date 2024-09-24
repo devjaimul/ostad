84f9b43c3cd6217b2aa61cd55a5f519fc3ce122a
Jaimul Islam Akik <105303801+devjaimul@users.noreply.github.com>
2024-09-25 00:17:58 +0600

    updated api without model

--- lib/constant/constamt.dart
+++
@@ -1,3 +0,0 @@
-
-
-const String baseUrl="http://152.42.163.176:2008/api/v1/";
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:ostad/views/product_list.dart';
+import 'package:ostad/ui/views/app.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,49 +8,12 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
-          useMaterial3: true,
-          appBarTheme: const AppBarTheme(
-            iconTheme: IconThemeData(color: Colors.white),
-            backgroundColor: Colors.teal,
-            foregroundColor: Colors.white,
-            centerTitle: true,
-          ),
-          inputDecorationTheme: InputDecorationTheme(
-              focusedBorder: OutlineInputBorder(
-                borderSide: const BorderSide(color: Colors.black),
-                borderRadius: BorderRadius.circular(20),
-              ),
-              enabledBorder: OutlineInputBorder(
-                borderSide: const BorderSide(color: Colors.black),
-                borderRadius: BorderRadius.circular(20),
-              ),
-              errorBorder: OutlineInputBorder(
-                borderSide: const BorderSide(color: Colors.red),
-                borderRadius: BorderRadius.circular(20),
-              ),
-              focusedErrorBorder: OutlineInputBorder(
-                borderSide: const BorderSide(color: Colors.red),
-                borderRadius: BorderRadius.circular(20),
-              )),
-          elevatedButtonTheme: ElevatedButtonThemeData(
-            style: ElevatedButton.styleFrom(
-                fixedSize: const Size.fromWidth(double.maxFinite),
-                padding: const EdgeInsets.all(18),
-                backgroundColor: Colors.teal,
-                foregroundColor: Colors.white,
-                shape: const RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(15)),
-                )),
-          )),
-      home: const ProductList(),
+      home: AppScreen(),
     );
   }
 }
+
